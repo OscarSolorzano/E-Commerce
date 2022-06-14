687609f317e8f5e16d7da287fe156b1be5014ebb
OscarSolorzano <oscar.solorzano98@gmail.com>
2022-06-13 21:58:46 -0500

    Creacion de Cards, inicio stock, estructura del proyecto

---
+++ java/pageGenerator.js
@@ -0,0 +1,24 @@
+"use strict"
+import { stock } from './stock.js';
+
+function generateCard(objectArray) {
+    let html = '';
+    for(let i = 0; i < objectArray.length; i++) {
+        html += `<div class="col-2-5 my-4">
+                        <div class="card">
+                            <img src="${objectArray[i].colors[0].pictures[0]}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                            <p>${objectArray[i].name}</p>
+                            <div class="color-container d-flex">
+                                <div class="color-circle mx-1 mb-1" style="background:${objectArray[i].colors[0].colorid}"></div>
+                                <div class="color-circle mx-1 mb-1" style="background:${objectArray[i].colors[1].colorid}"></div>
+                            </div>
+                            <p class="text-end">${objectArray[i].price}</p>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+    const container = document.getElementById('product-container');
+    container.innerHTML = html;
+}
+generateCard(stock);
---
+++ java/stock.js
@@ -0,0 +1,103 @@
+export let stock = 
+    [
+        {id: '1',
+        name: 'APE HEAD CREWNECK',
+        price: '$60',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
+            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            ]
+        },
+
+        {id: '2',
+        name: 'COLLEGE FULL ZIP HOODIE',
+        price: '$80',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
+            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            ]
+        },
+
+        {id: '1',
+        name: 'APE HEAD CREWNECK',
+        price: '$60',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
+            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            ]
+        },
+
+        {id: '2',
+        name: 'COLLEGE FULL ZIP HOODIE',
+        price: '$80',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
+            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            ]
+        },
+
+        {id: '1',
+        name: 'APE HEAD CREWNECK',
+        price: '$60',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
+            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            ]
+        },
+
+        {id: '2',
+        name: 'COLLEGE FULL ZIP HOODIE',
+        price: '$80',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
+            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            ]
+        },
+
+        {id: '1',
+        name: 'APE HEAD CREWNECK',
+        price: '$60',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
+            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            ]
+        },
+
+        {id: '2',
+        name: 'COLLEGE FULL ZIP HOODIE',
+        price: '$80',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
+            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            ]
+        },
+
+        {id: '1',
+        name: 'APE HEAD CREWNECK',
+        price: '$60',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
+            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            ]
+        },
+
+        {id: '2',
+        name: 'COLLEGE FULL ZIP HOODIE',
+        price: '$80',
+        description: 'Descripcion de la prenda',
+        colors:[
+            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
+            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            ]
+        },
+];
+
