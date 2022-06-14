74ddcc8c7d93ef65b4c5e76c0f0120c2f779a380
OscarSolorzano <oscar.solorzano98@gmail.com>
2022-06-14 12:21:23 -0500

    stock updated 15 products

--- java/stock.js
+++ java/stock.js
@@ -1,103 +1,145 @@
-export let stock = 
-    [
+export let stock = [
         {id: '1',
-        name: 'APE HEAD CREWNECK',
-        price: '$60',
-        description: 'Descripcion de la prenda',
+        name: '30TH TEE',
+        price: '$30',
+        description: 'Printed Logo T-shirt',
         colors:[
-            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
-            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            { color:'Black', colorid:'#0D0C11' , pictures: ['./Assets/1black1.png', './Assets/1black2.png']},
+            { color:'White', colorid:'#EBEAF1' ,pictures: ['./Assets/1white1.png', './Assets/1white2.png']}
             ]
         },
 
         {id: '2',
-        name: 'COLLEGE FULL ZIP HOODIE',
-        price: '$80',
-        description: 'Descripcion de la prenda',
+        name: 'CAMO BE@R COLLEGE TEE',
+        price: '$30',
+        description: 'Printed Logo T-shirt',
         colors:[
-            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
-            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            { color:'White', colorid: '#EBEAF1' ,pictures: ['./Assets/2white1.png', './Assets/2white2.png']},
+            { color:'Black', colorid:'#0D0C11' , pictures: ['./Assets/2black1.png', './Assets/2black2.png']}
             ]
         },
 
-        {id: '1',
+        {id: '3',
+        name: 'GRAPHIC LOOSE CUT TEE',
+        price: '$25',
+        description: 'Printed Logo cropped T-shirt',
+        colors:[
+            { color:'Sky', colorid:'#B1D3E4' , pictures: ['./Assets/3sky1.png', './Assets/3sky2.png']},
+            { color:'Pink', colorid:'#FD5410' ,pictures: ['./Assets/3orange1.png', './Assets/3orange2.png']}
+            ]
+        },
+
+        {id: '4',
+        name: 'BUTTON DOWN TOP',
+        price: '$35',
+        description: 'Button Down shirt',
+        colors:[
+            { color:'Purple', colorid:'#9B6D91' , pictures: ['./Assets/4purple1.png', './Assets/4purple2.png']},
+            { color:'Yellow', colorid: '#F0D9A0' ,pictures: ['./Assets/4yellow1.png', './Assets/4yellow2.png']}
+            ]
+        },
+
+        {id: '5',
+        name: 'GRAPHIC CROPPED TEE',
+        price: '$25',
+        description: 'Printed Logo cropped T-shirt',
+        colors:[
+            { color:'Navy', colorid:'#283A51' , pictures: ['./Assets/5navy1.png', './Assets/5navy2.png']},
+            { color:'White', colorid:'#EEF0F4' ,pictures: ['./Assets/5white1.png', './Assets/1white2.png']}
+            ]
+        },
+
+        {id: '6',
+        name: 'CARD CREWNECK',
+        price: '$60',
+        description: 'Printed Logo sweater',
+        colors:[
+            { color:'Grey', colorid:'#9A968E' , pictures: ['./Assets/6grey1.png', './Assets/5grey2.png']},
+            { color:'Black', colorid:'#0C0A0A' ,pictures: ['./Assets/6black1.png', './Assets/1black2.png']}
+            ]
+        },
+        {id: '7',
         name: 'APE HEAD CREWNECK',
         price: '$60',
-        description: 'Descripcion de la prenda',
+        description: 'Printed Logo sweater',
         colors:[
-            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
-            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
+            { color:'black', colorid:'#0C0A0A' , pictures: ['./Assets/7black1.png', './Assets/7black2.png']},
+            { color:'White', colorid:'#0D0C11' ,pictures: ['./Assets/7white1.png', './Assets/7white2.png']}
+            ]
+        },
+        {id: '8',
+        name: 'CHECK OVERSIZED SHIRT',
+        price: '$50',
+        description: 'Printed Logo buttom up shirt',
+        colors:[
+            { color:'Brown', colorid:'#A2735B' , pictures: ['./Assets/8brown1.png', './Assets/8brown2.png']},
+            { color:'Navy', colorid:'#262E44' ,pictures: ['./Assets/8navy1.png', './Assets/8navy2.png']}
+            ]
+        },
+        {id: '9',
+        name: 'CHAMBRAY RELAXED FIT SHIRT',
+        price: '$50',
+        description: 'Embroidered Logo cropped T-shirt',
+        colors:[
+            { color:'Grey', colorid:'#7591BC' , pictures: ['./Assets/9grey1.png', './Assets/9grey2.png']},
+            { color:'Sky', colorid:'#717675' ,pictures: ['./Assets/9sky1.png', './Assets/9sky2.png']}
+            ]
+        },
+        {id: '10',
+        name: 'SHARK LOOSE FIT MA-1',
+        price: '$100',
+        description: 'Embroidered Logo bomber jacket',
+        colors:[
+            { color:'Green', colorid:'#44412D' , pictures: ['./Assets/10green1.png', './Assets/10green2.png']},
+            { color:'Black', colorid:'#141318' ,pictures: ['./Assets/10black1.png', './Assets/10black2.png']}
             ]
         },
 
-        {id: '2',
-        name: 'COLLEGE FULL ZIP HOODIE',
-        price: '$80',
-        description: 'Descripcion de la prenda',
+        {id: '11',
+        name: 'AMO NYLON TWILL MA-1',
+        price: '$100',
+        description: 'Cammo print bomber jacket',
         colors:[
-            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
-            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
+            { color:'Green', colorid:'#686F3F' , pictures: ['./Assets/11green1.png', './Assets/11green2.png']},
+            { color:'Yellow', colorid:'#BFA669' ,pictures: ['./Assets/11yellow1.png', './Assets/11yellow.png']}
+            ]
+        },
+        {id: '12',
+        name: 'SHARK LOOSE FIT MA-2',
+        price: '$100',
+        description: 'Embroidered Logo bomber jacket',
+        colors:[
+            { color:'Black', colorid:'#242C2E' , pictures: ['./Assets/12black1.png', './Assets/12black2.png']},
+            { color:'Green', colorid:'#4E5A48' ,pictures: ['./Assets/12green1.png', './Assets/12green2.png']}
+            ]
+        },
+        {id: '13',
+        name: 'JAPAN SOUVENIR JACKET',
+        price: '$100',
+        description: 'Embroidered Logo jacket',
+        colors:[
+            { color:'Blue', colorid:'#3B6E9F' , pictures: ['./Assets/13blue1.png', './Assets/13blue2.png']},
+            { color:'Black', colorid:'#1B1B1D' ,pictures: ['./Assets/13black1.png', './Assets/13black2.png']}
+            ]
+        },
+        {id: '14',
+        name: 'RELAXED FIT MILITARY SHIRT',
+        price: '$100',
+        description: 'Printed Logo military jacket',
+        colors:[
+            { color:'Green', colorid:'#686F3F' , pictures: ['./Assets/14green1.png', './Assets/14green2.png']},
+            { color:'Black', colorid:'#BFA669' ,pictures: ['./Assets/14black1.png', './Assets/14yellow2.png']}
+            ]
+        },
+        {id: '15',
+        name: 'MULTI POCKET SHARK HOODIE JACKET',
+        price: '$100',
+        description: 'Printed Logo jacket',
+        colors:[
+            { color:'Purple', colorid:'#2E1D2A' , pictures: ['./Assets/15purple1.png', './Assets/15purple2.png']},
+            { color:'Black', colorid:'#181A1E' ,pictures: ['./Assets/15black1.png', './Assets/15black2.png']}
             ]
         },
 
-        {id: '1',
-        name: 'APE HEAD CREWNECK',
-        price: '$60',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
-            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
-            ]
-        },
 
-        {id: '2',
-        name: 'COLLEGE FULL ZIP HOODIE',
-        price: '$80',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
-            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
-            ]
-        },
-
-        {id: '1',
-        name: 'APE HEAD CREWNECK',
-        price: '$60',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
-            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
-            ]
-        },
-
-        {id: '2',
-        name: 'COLLEGE FULL ZIP HOODIE',
-        price: '$80',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
-            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
-            ]
-        },
-
-        {id: '1',
-        name: 'APE HEAD CREWNECK',
-        price: '$60',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Brown', colorid:'#CBB7A8' , pictures: ['./Assets/1Brown1.png', './Assets/1Brown2.png']},
-            { color:'Pink', colorid:'#D4B0B0' ,pictures: ['./Assets/1Pink1.png', './Assets/1Pink2.png']}
-            ]
-        },
-
-        {id: '2',
-        name: 'COLLEGE FULL ZIP HOODIE',
-        price: '$80',
-        description: 'Descripcion de la prenda',
-        colors:[
-            { color:'Grey', colorid:'#5A595E' , pictures: ['./Assets/2Grey1.png', './Assets/2Grey1.png']},
-            { color:'Sky', colorid: '#61B9D4' ,pictures: ['./Assets/2Blue1.png', './Assets/2Blue1.png']}
-            ]
-        },
-];
-
+]
